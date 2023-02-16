eea179b0b207f8dec6f97a9405c4ca47890e70e5
Iboro William <iboromw@gmail.com>
2023-02-16 15:45:11 -0800

    Last commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**my first c task**/
+
+#include <stdio.h>
+
+/**My main function**/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+/**Return description**/
+return (0);
+}
