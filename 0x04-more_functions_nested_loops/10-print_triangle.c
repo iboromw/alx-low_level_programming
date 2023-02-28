4108c8be80f99a3446ebf51a7475a0a5f1951fdd
Iboro William <iboromw@gmail.com>
2023-02-28 12:17:42 -0800

    Prime

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,14 +9,12 @@
 void print_triangle(int size)
 {
 int row, col;
-
 if (size > 0)
 {
 for (row = 1; row <= size; row++)
 {
 for (col = size - row; col > 0; col--)
 _putchar(' ');
-		
 for (col = 0; col < row; col++)
 _putchar('#');
 if (row == size)
