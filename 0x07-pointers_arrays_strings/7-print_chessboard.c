1b941b3bd1caef681a3989e1d575e486c108b28f
Iboro William <iboromw@gmail.com>
2023-03-06 18:30:17 -0800

    Last push

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints out a chessboard
- *
- * @a: chessboard to print, 2D array [8] columns
- *
- * Return: always void
+ * print_chessboard - prints the chessboard
+ * @a: the board to print
  */
+
 void print_chessboard(char (*a)[8])
 {
-int subC, primC = 0;
+int i, j;
 
-while (primC < 8)
+i = j = 0;
+while (i < 8)
 {
-subC = 0;
-while (subC < 8)
-_putchar(a[primC][subC++]);
-_putchar('\n');
-primC++;
+j = 0;
+while (j < 8)
+{
+_putchar(a[i][j]);
+j++;
 }
+_putchar('\n');
+i++;
+}
+
 }
