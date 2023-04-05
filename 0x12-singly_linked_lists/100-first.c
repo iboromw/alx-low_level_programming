049e55db0e58b98712e1329a396d06812b3a5a24
Iboro William <iboromw@gmail.com>
2023-04-05 15:19:27 -0700

    String commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * @head: main
+ * turtle - prints before main
+ * 
+ */
+
+void __attribute__ ((constructor)) turtle()
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
